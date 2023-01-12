f54b50d7cbfc07bc049db32e3962119606993a37
tmdgus1715 <tmdgus1715@gamil.com>
2023-01-12 13:14:25 +0900

    s

--- src/main/java/org/example/util/security/Encryption.java
+++
@@ -1,8 +0,0 @@
-package org.example.util.security;
-
-public interface Encryption {
-    public String encrypt(String password);
-
-    public void checkPassword(String plainPassword, String hashedPassword);
-
-}
